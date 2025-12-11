3a942979c772cdd5545bfc576b31bf05b738db42
Soeren Wegener <wegener92@gmail.com>
2025-12-12 00:55:21 +0100

    Comment code back in

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # configure motor speed
-# motor = LED(4)
-# motor.blink(on_time=0.0001, off_time=0.0001)
-#
-# motor_enable = OutputDevice(27)
+motor = LED(4)
+motor.blink(on_time=0.0001, off_time=0.0001)
+
+motor_enable = OutputDevice(27)
 
 @app.get("/on")
 def motor_on():
